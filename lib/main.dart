32410b18918b18c32fb7dcbe3e3f6c994af0c592
ArifZakwan <arifzakwanahmadhisham@gmail.com>
2021-09-23 14:21:42 +0800

    add appbar

--- lib/main.dart
+++ lib/main.dart
@@ -4,4 +4,24 @@ void main() {
   runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Favourite Anime List'),
+        ),
+        body: Column(
+          children: [
+            
+          ],
+        ),
+      )
+    );
+  }
+}
+
+
+
 
