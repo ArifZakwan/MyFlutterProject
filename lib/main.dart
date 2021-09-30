be241c17b5e02298de9266d2e6347be41ef8c48d
ArifZakwan <arifzakwanahmadhisham@gmail.com>
2021-09-30 16:25:15 +0800

    make appbar

--- lib/main.dart
+++ lib/main.dart
@@ -3,147 +3,32 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class Manga {
-  final String mangaTitle;
-  final String genre;
-  final String author;
-  final String chapter;
-  final String status;
-  final String image;
-  Manga(this.mangaTitle, this.genre, this.author, this.chapter, this.status,
-      this.image);
 
-  static List<Manga> getMangas() {
-    List<Manga> info = <Manga>[];
-    info.add(Manga(
-        "Shingeki No Kyojin",
-        "Action, Drama, Fantasy, Historical, Horror, Mystery, Shounen, Tragedy.",
-        "Isayama Hajime",
-        "Chapter 139",
-        "Finished",
-        "shingeki_no_kyojin.jpg"));
-    info.add(Manga(
-        "Kimetsu No Yaiba",
-        "Action, Adventure, Fantasy, Historical, Shounen.",
-        "Koyoharu Gotouge",
-        "Chapter 206",
-        "Finished",
-        "kimetsu_no_yaiba.jpg"));
-    return info;
-  }
-}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MyFavourite Manga',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: ListMangaPage(),
-    );
-  }
-}
-
-class ListMangaPage extends StatelessWidget {
-  ListMangaPage({Key? key}) : super(key: key);
-  final info = Manga.getMangas();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text("Favourite Manga List")),
-        body: ListView.builder(
-          itemCount: info.length,
-          itemBuilder: (context, index) {
-            return GestureDetector(
-              child: MangaBox(item: info[index]),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MangaPage(item: info[index]),
-                  ),
-                );
-              },
-            );
-          },
-        )
-      );
-  }
-}
-
-class MangaPage extends StatelessWidget {
-  MangaPage({Key? key, required this.item}) : super(key: key);
-  final Manga item;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(this.item.mangaTitle),
-      ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(0),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Image.asset(
-                  "assets/" + this.item.image,
-                  alignment: Alignment.center,
-                ),
-                Expanded(
-                    child: Container(
-                        padding: EdgeInsets.all(5),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Text(this.item.mangaTitle,
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text(this.item.genre),
-                            Text(this.item.author),
-                            Text(this.item.chapter),
-                            Text(this.item.status),
-                          ],
-                        )))
-              ]),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          leading: Icon(Icons.lock_outline),
+          titleSpacing: -10,
+          title: Text('arf_zkwn'),
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {}, 
+              icon: Icon(Icons.add_box_outlined),
+            ),
+            IconButton(
+              onPressed: () {}, 
+              icon: Icon(Icons.settings),
+            )
+          ],
         ),
       ),
     );
   }
 }
 
-class MangaBox extends StatelessWidget {
-  MangaBox({Key? key, required this.item}) : super(key: key);
-  final Manga item;
-
-  Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.all(2),
-        height: 140,
-        child: Card(
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Image.asset("assets/" + this.item.image),
-                Expanded(
-                    child: Container(
-                        padding: EdgeInsets.all(5),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Text(this.item.mangaTitle,
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            Text(this.item.genre),
-                            Text(this.item.author),
-                            Text(this.item.chapter),
-                            Text(this.item.status),
-                          ],
-                        )))
-              ]),
-        ));
-  }
-}
