b9f238cda97304022a7ca5aee5603ecea43bf1c0
ArifZakwan <arifzakwanahmadhisham@gmail.com>
2021-10-01 17:01:15 +0800

    make ui insta clone

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,13 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MyFavourite Manga',
       home: Scaffold(
+        backgroundColor: Colors.black,
         appBar: AppBar(
           backgroundColor: Colors.black,
           leading: Icon(Icons.lock_outline),
@@ -18,17 +17,78 @@ class MyApp extends StatelessWidget {
           title: Text('arf_zkwn'),
           actions: <Widget>[
             IconButton(
-              onPressed: () {}, 
+              onPressed: () {},
               icon: Icon(Icons.add_box_outlined),
             ),
             IconButton(
-              onPressed: () {}, 
-              icon: Icon(Icons.settings),
+              onPressed: () {},
+              icon: Icon(Icons.menu),
             )
           ],
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Column(
+              children: [ 
+                CircleAvatar(
+                backgroundImage: AssetImage('assets/kimetsu_no_yaiba.jpg'),
+                radius: 30,
+            ), ]
+            ),
+            Column(
+              children: [
+                Text('5', style: TextStyle(color: Colors.white, fontSize: 20),), 
+                Text('Posts', style: TextStyle(color: Colors.white),)
+              ],
+            ),
+            Column(
+              children: [
+                Text('5', style: TextStyle(color: Colors.white, fontSize: 20),), 
+                Text('Followers', style: TextStyle(color: Colors.white),)
+              ],
+            ),
+            Column(
+              children: [
+                Text('1726', style: TextStyle(color: Colors.white, fontSize: 20),), 
+                Text('Following', style: TextStyle(color: Colors.white),)
+              ],
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.black,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(
+                Icons.home_outlined, 
+                color: Colors.white,
+                size: 30,
+              ),
+              Icon(
+                Icons.search, 
+                color: Colors.white,
+                size: 30,
+              ),
+              Icon(
+                Icons.movie_outlined, 
+                color: Colors.white,
+                size: 30,
+              ),
+              Icon(
+                Icons.shopping_bag_outlined,
+                color: Colors.white,
+                size: 30,
+              ),
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/kimetsu_no_yaiba.jpg'),
+                radius: 15,
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
